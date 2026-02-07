8d31bcad3735073a1d454bae049a2f10e249908a
= <=>
2026-02-06 23:38:36 -0500

    frontend working

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
